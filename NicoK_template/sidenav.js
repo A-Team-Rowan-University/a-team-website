e585a6796a817fc11311e4a237e757aca92da81b
nicokaegi <kaeginick63@gmail.com>
2019-04-14 18:03:12 -0400

    uploaded new template file

---
+++ NicoK_template/sidenav.js
@@ -0,0 +1,37 @@
+function opennav() {
+  document.getElementById("sidenav").style.width = "24em";// slides out side nav menu
+
+  document.getElementById("linksDiv")
+}
+
+function sideNavDropDown(){
+
+  var acc = document.getElementsByClassName("accordionMenu");
+  var i; /* finds every element with the class id accodian and addes them to a list */
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {/* makes said elements open/close their respective drop down menu when clicked*/
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+    });
+  }
+
+}
+
+function closenav()
+{
+  document.getElementById("sidenav").style.width = "0em";
+}
+
+function sideUpdate(){
+
+  document.getElementById("container").innerHTML = "bloop";
+
+
+
+}
