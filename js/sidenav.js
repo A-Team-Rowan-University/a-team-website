286da22c6b1428ae0df24e4edb71021397b027d0
nicokaegi <kaeginick63@gmail.com>
2019-04-14 20:51:41 -0400

    added links to existing users pages

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,11 +1,39 @@
 function opennav() {
   document.getElementById("sidenav").style.width = "24em";// slides out side nav menu
 
-  document.getElementById("linksDiv")
+}
+
+function updatelinks() {
+
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  alert(page);
+
+/*  document.getElementById("sidenav") = `<div id="sidenav" class="sidenav">
+  <div id = closebutton>
+    <a href="javascript:void(0)" class="closebtn" onclick="closenav()">&times;</a>
+  </div>
+  <div id = linksDiv>
+    <a href="#" class = "dropdown">Index</a>
+    <button class="accordionMenu"><span class = dropdown>People</span></button>
+    <div class="panel">
+      <a href="PeopleDatabase/AllUsers.html">All Users</a>
+      <a href="PeopleDatabase/SearchUser">Search User</a>
+      <a href="PeopleDatabase/AddUsers.html">Add User</a>
+      <a href="#">Edit User</a>
+    </div>
+    <button class="accordionMenu"><span class = dropdown>Chemicals</span></button>
+    <div class="panel">
+      <a href="PeopleDatabase/">All Chemicals</a>
+      <a href="#">Search Chemical</a>
+      <a href="#">Add Chemical</a>
+      <a href="#">Edit Chemical</a>
+    </div>
+    </div>`*/
+
 }
 
 function sideNavDropDown(){
-
   var acc = document.getElementsByClassName("accordionMenu");
   var i; /* finds every element with the class id accodian and addes them to a list */
 
@@ -22,6 +50,7 @@ function sideNavDropDown(){
   }
 
 }
+
 
 function closenav()
 {
