ef117c272c214ded019a982dde21e16950b8c27e
John McAvoy <mcavoyj5@students.rowan.edu>
2019-05-27 18:45:21 -0400

    Adds navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,34 @@
 import React, { MouseEvent } from 'react'
-import 'purecss/build/pure.css';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import logo from '../resources/images/A-Team-Logo.png'
+import {SignedInUser} from './SignIn'
 
-export default class Nav extends React.Component {
+export default class Navigation extends React.Component<NavigationProps> {
+
+    componentWillMount() {
+        
+    }
+
     render() {
         return (
-            <header>
-                <div className="container">
-                    <nav>
-                        <button className="navbutton">&#9776;</button>
-                    </nav>
-                    <div id="branding">
-                        <img id="logo" src="media/A-Team-Logo.png" />
-                        <h1>Template</h1>
-                    </div>
-                </div>
-            </header>
-        )
-    }
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="/"> <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />
+                    {' A-Team at Rowan University'}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/tests">Tests</Nav.Link>
+                        <Nav.Link href="/users">Users</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="/sign_in">SignIn</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )}
+}
+
+interface NavigationProps {
+    user?: SignedInUser
 }
