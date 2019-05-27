3b4959e4018f5734fa7b1505cad19fb95988aafc
John McAvoy <mcavoyj5@students.rowan.edu>
2019-05-27 16:13:14 -0400

    Add fetch to SafetyTest

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import SafetyTest, {SafetyTestProps}  from './SafetyTest'
+import SafetyTest, {SafteyTestProps} from './SafetyTest'
 import Question, {QuestionProps}  from './Question'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
-const q1Props: QuestionProps = {
-    id: 1,
-    question: 'What is 2+2?',
-    choices: [
-        {id:1 , text:'1'},
-        {id:2 , text:'banana'},
-        {id:3 , text:'chimpanzee'},
-        {id:4 , text:'4'},
-    ]
-}
-
-const q2Props: QuestionProps = {
-    id: 2,
-    question: 'What color is the sky?',
-    choices: [
-        {id:1 , text:'green'},
-        {id:2 , text:'true'},
-        {id:3 , text:'a ring-toed lemur'},
-        {id:4 , text:'blue'},
-    ]
-}
 
 export default class App extends React.Component {
     render() {
@@ -34,10 +12,7 @@ export default class App extends React.Component {
                 <Row className="justify-content-center">
                     <h1>Safety Test</h1>
                 </Row>
-                <Row>
-                    <Question {...q1Props}/>
-                    <Question {...q2Props}/>
-                </Row>
+                <SafetyTest />
             </Container>
         )
     }
--- src/components/SafetyTest.tsx
+++ src/components/SafetyTest.tsx
@@ -1,16 +1,68 @@
-import React from 'react'
+import React from 'react';
+import Question, {QuestionProps}  from './Question'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import config from '../config'
 
-export default class SafetyTest extends React.Component<SafetyTestProps> {
+const q1Props: QuestionProps = {
+    id: 1,
+    question: 'What is 2+2?',
+    choices: [
+        {id:1 , text:'1'},
+        {id:2 , text:'banana'},
+        {id:3 , text:'chimpanzee'},
+        {id:4 , text:'4'},
+    ]
+}
+
+const q2Props: QuestionProps = {
+    id: 2,
+    question: 'What color is the sky?',
+    choices: [
+        {id:1 , text:'green'},
+        {id:2 , text:'true'},
+        {id:3 , text:'a ring-toed lemur'},
+        {id:4 , text:'blue'},
+    ]
+}
+
+export default class SafetyTest extends React.Component<SafteyTestProps, SafetyTestState> {
+
+    async componentDidMount() {
+        const headers = new Headers();
+        headers.append('id_token', this.props.user.id_token)
+
+        const init: RequestInit = {
+            method: 'GET',
+            headers
+        }
+
+        const response = await fetch(`${config.api_url}/test_sessions/1/open`, init)
+        console.log(response)
+    }
+
     render() {
         return (
-            <div className="test">
-                <h1>Safety Test</h1>
-                {this.props.questions}
-            </div>
+            <Container className="App header justify-content-center">
+                <Row className="justify-content-center">
+                    <h1>Safety Test</h1>
+                </Row>
+                <Row>
+                    <Question {...q1Props}/>
+                    <Question {...q2Props}/>
+                </Row>
+            </Container>
         )
     }
 }
 
-export interface SafetyTestProps {
-    questions: any;
+interface SafetyTestState {
+    questions: Question[]
+}
+
+export interface SafteyTestProps {
+    user: {
+        id_token: string
+    }
 }
