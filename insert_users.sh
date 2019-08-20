6465f83f5ad6b2a588c5b14728a84ee9c5d4d755
John McAvoy <mcavoyj5@students.rowan.edu>
2019-08-20 18:22:53 -0400

    Parallel insert users

--- insert_users.sh
+++ insert_users.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-URL="localhost/api/v1"
+export URL="localhost/api/v1"
 
-ID_TOKEN=$1
+export ID_TOKEN=$1
 
 function insert_user() {
     echo
     echo Inserting $1
     echo curl --data "'${1}'" -H id_token:$ID_TOKEN $URL/users/
-    curl --data "'${1}'" -H id_token:$ID_TOKEN $URL/users/
+    curl --data "${1}" -H id_token:$ID_TOKEN $URL/users/
 }
 
-while IFS= read user; do
-    insert_user "${user}"
-done
+export -f insert_user
+parallel insert_user {}
