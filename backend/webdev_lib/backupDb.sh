d61640ba8b0e1c5bf46259eebfef3c9e6a1c925b
John McAvoy <mcavoyj5@students.rowan.edu>
2019-09-11 23:22:33 -0400

    Add info on how to install gdrive

--- backend/webdev_lib/backupDb.sh
+++ backend/webdev_lib/backupDb.sh
@@ -5,4 +5,11 @@ OUT_FILE="${DATE}_backup.sql"
 
 docker exec ateamwebsite_db_1 /usr/bin/mysqldump -uroot -papple1 web_dev > $OUT_FILE
 echo $OUT_FILE
-gdrive upload $OUT_FILE
+
+if hash gdrive 2>/dev/null; then
+    gdrive upload $OUT_FILE
+else
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    >&2 echo -e $RED gdrive is not installed. For more information go to https://github.com/gdrive-org/gdrive#installation $NC
+fi
