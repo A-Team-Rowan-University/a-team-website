4f907698a1c6056aa0161a76b24952e32674b288
John McAvoy <mcavoyj5@students.rowan.edu>
2019-09-11 23:22:18 -0400

    Update docker container name

--- backend/webdev_lib/backupDb.sh
+++ backend/webdev_lib/backupDb.sh
@@ -3,6 +3,6 @@
 DATE=`date '+%Y-%m-%d-%H-%M-%s'`
 OUT_FILE="${DATE}_backup.sql"
 
-docker exec resourcewebsite_db_1 /usr/bin/mysqldump -uroot -papple1 web_dev > $OUT_FILE
+docker exec ateamwebsite_db_1 /usr/bin/mysqldump -uroot -papple1 web_dev > $OUT_FILE
 echo $OUT_FILE
 gdrive upload $OUT_FILE
